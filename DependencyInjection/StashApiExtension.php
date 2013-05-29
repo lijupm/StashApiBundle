cafb86a739d71ba48e9f786fe5cbd78e39905f10
Kees Schepers <kees.schepers@medicore.nl>
2013-05-29 15:53:59 +0200

    fixed some namings

--- DependencyInjection/StashApiExtension.php
+++ DependencyInjection/StashApiExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AtlasCliBundle\DependencyInjection;
+namespace StashApiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class AtlasCliExtension extends Extension
+class StashApiExtension extends Extension
 {
     /**
      * {@inheritDoc}
@@ -25,8 +25,8 @@ class AtlasCliExtension extends Extension
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
     }
-    
+
     public function getAlias() {
-       return 'atlas_cli';
+       return 'stash_api';
     }
 }
